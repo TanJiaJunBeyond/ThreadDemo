ef8242031e048731244c4af4e817fe3ee4c9ed24
谭嘉俊 <1120571286@qq.com>
2020-08-31 01:12:29 +0800

    添加"Java线程基础"文章中的"生产者消费者问题"的示例代码。

---
+++ src/producerconsumerproblem/ProducerConsumerProblemTest.java
@@ -0,0 +1,74 @@
+package producerconsumerproblem;
+
+/**
+ * Created by TanJiaJun on 2020/8/30.
+ */
+class ProducerConsumerProblemTest {
+
+    // 生产者线程
+    private static class Producer implements Runnable {
+
+        private Repository repository;
+
+        Producer(Repository repository) {
+            this.repository = repository;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    // 让生产者线程睡眠一秒
+                    Thread.sleep(1000);
+                    // 调用存储库的produce()方法，生产者生产产品
+                    repository.produce();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        }
+
+    }
+
+    // 消费者线程
+    private static class Consumer implements Runnable {
+
+        private Repository repository;
+
+        Consumer(Repository repository) {
+            this.repository = repository;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    // 让消费者线程睡眠三秒
+                    Thread.sleep(3000);
+                    // 调用存储库的consume()方法，消费者消费产品
+                    repository.consume();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Repository repository = new Repository();
+
+        // 创建三个生产者线程，并且让它们运行
+        for (int i = 0; i < 3; i++) {
+            new Thread(new Producer(repository)).start();
+        }
+
+        // 创建三个消费者线程，并且让它们运行
+        for (int i = 0; i < 3; i++) {
+            new Thread(new Consumer(repository)).start();
+        }
+    }
+
+}
---
+++ src/producerconsumerproblem/Repository.java
@@ -0,0 +1,53 @@
+package producerconsumerproblem;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by TanJiaJun on 2020/8/30.
+ */
+class Repository {
+
+    // 队列的最大容量是10
+    private static final int MAX_SIZE = 10;
+    private final Queue<Object> queue = new LinkedList<>();
+
+    void produce() {
+        synchronized (queue) {
+            while (queue.size() == MAX_SIZE) {
+                try {
+                    System.out.println("生产者（线程名字：" + Thread.currentThread().getName() + "）：存储库已满");
+                    // 当队列满了后，调用queue的wait()方法，生产者线程被阻塞，并且释放queue的监视器锁
+                    queue.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            // 如果队列还没满，就创建新的Object对象，并且在队尾入列
+            queue.offer(new Object());
+            System.out.println("生产者（线程名字：" + Thread.currentThread().getName() + "）：生产了一个产品");
+            // 通知其他生产者线程和消费者线程
+            queue.notifyAll();
+        }
+    }
+
+    void consume() {
+        synchronized (queue) {
+            while (queue.size() == 0) {
+                try {
+                    System.out.println("消费者（线程名字：" + Thread.currentThread().getName() + "）：存储库是空");
+                    // 当队列空了后，调用queue的wait()方法，消费者线程被阻塞，并且释放queue的监视器锁
+                    queue.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            // 如果队列存在元素，就将队头元素出列
+            queue.poll();
+            System.out.println("消费者（线程名字：" + Thread.currentThread().getName() + "）：消费了一个产品");
+            // 通知其他消费者线程和生产者线程
+            queue.notifyAll();
+        }
+    }
+
+}
