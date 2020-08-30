afa338ef7a2dbe4d8164e99a7c38b56dd59a897b
谭嘉俊 <1120571286@qq.com>
2020-08-30 13:50:57 +0800

    添加"Java线程基础"中的"线程创建和运行"的示例代码。

--- src/TanJiaJunFutureTaskTest.java
+++ src/TanJiaJunFutureTaskTest.java
@@ -17,10 +17,13 @@ class TanJiaJunFutureTaskTest {
     }
 
     public static void main(String[] args) {
+        // 创建FutureTask对象
         FutureTask<String> futureTask = new FutureTask<>(new TanJiaJunCallable());
+        // 创建线程
         Thread thread = new Thread(futureTask);
         thread.start();
         try {
+            // 等待任务执行完毕，并且得到返回值
             String result = futureTask.get();
             System.out.println(result);
         } catch (InterruptedException | ExecutionException e) {
